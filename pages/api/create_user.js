e744849d37e2b829b9de56808cd3063da789dfa6
montheramer <monther.amer48@gmail.com>
2021-02-07 23:44:23 +0200

    ADD errors handler to POST /api/create_user

--- pages/api/create_user.js
+++ pages/api/create_user.js
@@ -1,5 +1,13 @@
 export default (req, res) => {
-  let { firstName, lastName } = req.body;
-  console.log("BBBBBBBBBBbb ", req.body);
-  res.status(200).json({ payload: `${firstName.toUpperCase()} ${lastName.toUpperCase()}` });
+  // only post request allowed
+  if (req.method === "POST") {
+    let { firstName, lastName } = req.body;
+    // handle missing params
+    if (!firstName) res.status(400).json({ error: `firstName is required parameter!` });
+    if (!lastName) res.status(400).json({ error: `lastName is required parameter!` });
+
+    res.status(200).json({ payload: `${firstName.toUpperCase()} ${lastName.toUpperCase()}` });
+  } else {
+    res.status(404).json({ error: `${req.method} ${req.url} does not found` });
+  }
 };
